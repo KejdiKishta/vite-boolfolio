1cd58d5be60ad541eb118db8d005fa38e212d1c6
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-06-30 16:50:02 +0200

    add: cards with projects

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,20 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './ProjectCard.vue'
 
 export default {
+    components: {
+        ProjectCard,
+    },
     data() {
         return {
-
+            // variabile da riempire con axios
+            projects: [],
         }
     },
     created() {
         axios.get("http://127.0.0.1:8000/api/test").then((resp) => {
+            // salvo i dati nella varibile iniziale
             this.projects = resp.data.projects;
             // console.log(this.projects);
         })
@@ -17,9 +23,19 @@ export default {
 </script>
 
 <template>
-    
+    <h1>ciao</h1>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-3" v-for="project in projects" :key="project.id">
+                <ProjectCard :project="project" />
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped lang="scss">
-
+.col-3 > * {
+    aspect-ratio: 1;
+}
 </style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    props: {
+        project: Object,
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card m-2">
+        <div class="card-body">
+            <h5 class="card-title">{{ project.title }}</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">{{ project.type.name }}</h6>
+            <h6 class="card-subtitle mb-2 text-body-secondary">{{ project.owner }}</h6>
+            <p class="card-text">{{ project.description }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
