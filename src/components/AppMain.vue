09fec88949c6616786dc74ef80b057172bc54eeb
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-06-30 16:00:11 +0200

    feat: project scaffoldin add: axios get with backend data

---
+++ src/components/AppMain.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    created() {
+        axios.get("http://127.0.0.1:8000/api/test").then((resp) => {
+            this.projects = resp.data.projects;
+            // console.log(this.projects);
+        })
+    }
+}
+</script>
+
+<template>
+    
+</template>
+
+<style scoped lang="scss">
+
+</style>
